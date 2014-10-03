d8cd7b16b7e6499ff107bfba31f0cfbcf3c4a504
Joe Pistone <jpistone@etsy.com>
2014-10-03 10:52:04 -0400

    Cleaned up a bit, removed any of my path information. Then added a line of context to the top of the email.

--- forward.sh
+++ forward.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-
-#logging 
+#Very Dirty script to
+#alert if someone is forwarding
+#mail to another account.
+##################################
 
 #clean up the log each time.
-cd /Users/jpistone/gam/
+cd /path/to/gam_dir
 rm forward_logging.log
 rm forward_alert.log
 touch forward_alert.log
 touch forward_logging.log
 
 #GAM command dumps to log file
-python /Users/jpistone/gam/gam.py all users show forward > /Users/jpistone/gam/forward_logging.log
+python /path/to/gam_dir/gam.py all users show forward > /path/to/log/in/gam_dir/gam/forward_logging.log
 
-
+#Cats the above log file and removes all false values
 cat forward_logging.log | grep -v "Enabled:false" > forward_alert.log
 
-mail -s "Daily Forward Email Alert" jpistone@etsy.com < forward_alert.log
+#Adds some context to the body of the email.
+sed -i '1s/^/The Following Mailboxes are being forwarded:\n/' forward_alert.log
+
+#The internet is made of cats!
+mail -s "netsec: Daily Forward Report" mailtoaddress < forward_alert.log
